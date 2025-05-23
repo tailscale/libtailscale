134bba07dd45afa9ca4287f590b4edc0d3e6db49
Chris Hayen <chayen@gmail.com>
2025-05-23 15:09:19 -0400

    echo

---
+++ rust/tsnet/examples/echo.rs
@@ -0,0 +1,34 @@
+use std::{
+    io::{Read, Write},
+    net::TcpStream,
+    os::fd::FromRawFd,
+};
+use tsnet::TSNet;
+
+fn main() -> Result<(), String> {
+    let config = tsnet::ConfigBuilder::new().ephemeral(true).build()?;
+
+    let mut ts = TSNet::new(config)?;
+    ts.up()?;
+
+    let listener = ts.listen("tcp", ":1999")?;
+
+    loop {
+        let conn = ts.accept(listener).unwrap();
+        let mut stream = unsafe { TcpStream::from_raw_fd(conn) };
+        let remote_addr = ts.get_remote_addr(conn, listener).unwrap();
+        let mut buf = [0; 1024];
+
+        println!("connection from: {}", remote_addr);
+
+        while let Ok(n) = stream.read(&mut buf) {
+            if n == 0 {
+                break;
+            }
+            stream.write_all(&buf[..n]).unwrap();
+            stream.flush().unwrap();
+        }
+
+        return Ok(());
+    }
+}
