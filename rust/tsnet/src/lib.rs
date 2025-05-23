0245190a90c212c6f809c0153eead150074a3889
Chris Hayen <chayen@gmail.com>
2025-05-23 15:01:23 -0400

    import bindings

--- rust/tsnet/src/lib.rs
+++ rust/tsnet/src/lib.rs
@@ -1,14 +1,4 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+/// Raw bindings for libtailscale
+mod bindings {
+    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 }
