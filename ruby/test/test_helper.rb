7583f10fdc352f795af9d3ea70c364b46f02273c
James Tucker <jftucker@gmail.com>
2026-02-06 15:24:16 -0800

    ruby: fix tests using cmd/testcontrol
    The test helper ends up starting a single global instance of testcontrol
    which is a bit of a hack, but signal handling conflicts between the
    runtimes are otherwise problematic.
    
    Fixes tailscale/tailscale#18635

--- ruby/test/test_helper.rb
+++ ruby/test/test_helper.rb
@@ -3,4 +3,49 @@
 # frozen_string_literal: true
 
 require "tailscale"
-require "minitest/autorun"
+
+# start a single global testcontrol instance which is far from a best practice,
+# but this avoids some issues with signal propagation between ruby, threads, and
+# the go runtime.
+#
+# grumpy. https://github.com/golang/go/issues/40467
+unless system(*"go install tailscale.com/cmd/testcontrol")
+  raise "failed to go install testcontrol"
+end
+gobin = `go env GOBIN`.strip
+if gobin == ""
+  gobin = `go env GOPATH`.strip + "/bin"
+end
+
+path = gobin + "/testcontrol"
+if !File.executable? path
+  raise "#{path} is not an executable"
+end
+pid = Process.spawn(path)
+at_exit do
+  begin
+    Process.kill(0, pid)
+  rescue Errno::ESRCH
+    raise "testcontrol exited prematurely"
+  end
+  Process.kill(:TERM, pid)
+  Process.wait(pid)
+end
+$testcontrol_url = "http://127.0.0.1:9911"
+require "net/http"
+attempts = 0
+until (Net::HTTP::get(URI.parse($testcontrol_url + "/key")) rescue nil)
+  sleep 0.001
+  attempts+=1
+  begin
+    Process.kill(0, pid)
+  rescue Errno::ESRCH
+    raise "testcontrol exited prematurely"
+  end
+  if attempts == 10000
+    raise "timed out waiting for testcontrol http to start"
+  end
+end
+
+require "minitest"
+Minitest.autorun
